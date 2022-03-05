2190dba912c52ba8fe361e49cae0ad17e50106e0
ZHOU <510878689@qq.com>
2022-03-05 16:22:41 +0800

    修改网站logo、favicon和首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,38 @@
 module.exports = {
-  // base: '/vuepress-note/', // 部署站点的基础路径
+  base: '/vuepress-note/', // 部署站点的基础路径
   lang: 'zh-CN',
   title: 'NOTE',
-  description: '前端笔记',
+  description: '我的前端笔记',
+  head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
   theme: '@vuepress/theme-default',
   themeConfig: {
-    logo: 'https://vuejs.org/images/logo.png',
+    logo: '/images/logo.png',
     repo: 'cafehaus', // 你的仓库
     repoLabel: 'GitHub', // 导航栏上的文本
     docsRepo: 'cafehaus/vuepress-note',
     docsDir: 'docs/note',
     editLink: false, // 是否启用 编辑此页链接
+    lastUpdatedText: '最近更新',
+    contributorsText: '贡献者',
+    sidebarDepth: 0,
     navbar: [
       // { text: '关于我', link: '/about/' }
     ],
     sidebar: [
-      // { text: '首页', link: '/' },
+      { text: '首页', link: '/' },
+      {
+        text: 'vue',
+        collapsible: true,
+        children: [
+          { text: '用vuepress2搭建自己的github网站', link: '/note/vue/用vuepress2搭建自己的github网站/index.md' },
+        ]
+      },
       {
         text: 'uni',
         collapsible: true,
         children: [
-          { text: '安卓模拟器接口抓包教程', link: '/note/uni/android-proxy/index.md' },
+          { text: '开发字节抖音小程序踩坑记', link: '/note/uni/开发字节抖音小程序踩坑记/index.md' },
+          { text: '安卓模拟器接口抓包教程', link: '/note/uni/安卓模拟器接口抓包教程/index.md' },
         ]
       },
     ]
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,7 @@
-# Hello
+## 笔记分类
+
+### vue
+* [用vuepress2搭建自己的github网站](./note/vue/用vuepress2搭建自己的github网站/index.md)
+### uni
+* [开发字节抖音小程序踩坑记](./note/uni/开发字节抖音小程序踩坑记/index.md)
+* [安卓模拟器接口抓包教程](./note/uni/安卓模拟器接口抓包教程/index.md)
